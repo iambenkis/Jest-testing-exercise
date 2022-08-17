968f4c53cfc95677054a3296a62b6ec7a54cf94a
benjamin kisenge <benkisenge03@gmail.com>
2022-08-17 10:41:54 +0200

    Create reverseString Function

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+// Task 1
 function stringLength(string) {
      if(string.length >=1 && string.length <=10){
-        return "Yes"
+        return 'Yes'
      } else  {
         throw "Check your string"
      }
 }
-  module.exports = stringLength;
+module.exports = stringLength;
--- index.test.js
+++ index.test.js
@@ -1,5 +1,10 @@
-const sum = require('./index');
+const lengthOf = require('./index');
+const reverseOf = require('./index2');
 
 test('ben must return yes', () => {
-  expect(sum('ben')).toBe('Yes');
+  expect(lengthOf('ben')).toBe('Yes');
 });
+
+test('ben must return neb' , () => {
+    expect(reverseOf('ben')).toBe('neb');
+})
