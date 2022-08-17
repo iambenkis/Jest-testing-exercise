26ea5df8be8aec288adb872a28ae5453d450da0d
benjamin kisenge <benkisenge03@gmail.com>
2022-08-17 11:09:32 +0200

    Create calculator

--- index.test.js
+++ index.test.js
@@ -1,5 +1,6 @@
 const lengthOf = require('./index');
 const reverseOf = require('./index2');
+const myCalculator = require('./index3');
 
 test('ben must return yes', () => {
   expect(lengthOf('ben')).toBe('Yes');
@@ -8,3 +9,10 @@ test('ben must return yes', () => {
 test('benjamin must return nimajneb' , () => {
     expect(reverseOf('benjamin')).toBe('nimajneb');
 })
+
+describe('my calculator', () => {
+    const calculator = new myCalculator(3,2);
+    test('3 + 2 is 5', () => {
+      expect(calculator.add()).toBe(5);
+    });
+});
---
+++ index3.js
@@ -0,0 +1,24 @@
+class calculator {
+    constructor(a,b){
+        this.a = a;
+        this.b = b;
+    }
+    add = () => {
+        return this.a + this.b;
+    }
+    subtract = () => {
+        return this.a>this.b? this.a-this.b: this.b-this.a;
+    }
+    
+    multiply = () => {
+        return this.a * this.b;
+    }
+    
+    divide = () => {
+        return this.a!==0 && this.b!==0 ? this.a/this.b:'E';
+    }
+}
+
+    
+
+module.exports = calculator;
